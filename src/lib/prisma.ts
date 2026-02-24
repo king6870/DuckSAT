4c0fe730023d27399fbf58ac017597e0204d61a5
king6870 <148512407+king6870@users.noreply.github.com>
2026-02-23 21:25:19 -0800

    Change PrismaClient import path to @prisma/client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import path from 'path'
 import fs from 'fs'
 
@@ -22,7 +22,5 @@ logActiveEnvFile();
 
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
 export const prisma = globalForPrisma.prisma || new PrismaClient();
-
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
