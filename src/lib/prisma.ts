fda18a695ef1079872f6bd02abc61f1b2e15c505
king6870 <kingjacobisthegoat@gmail.com>
2026-02-24 15:17:08 -0800

    fix: Remove datasource configuration from PrismaClient initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -22,11 +22,5 @@ logActiveEnvFile();
 
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
