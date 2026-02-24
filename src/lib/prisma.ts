c139a13cfbcb8f08a123e56a9db8401a1279987f
king6870 <kingjacobisthegoat@gmail.com>
2026-02-24 15:39:50 -0800

    fix: Remove SQL Server adapter initialization from PrismaClient

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,11 +2,9 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-let adapter: any;
 if (typeof window === 'undefined') {
   // Use eval to prevent Webpack from bundling
-  adapter = eval("require('@prisma/client/adapter-sqlserver')");
 }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient(adapter ? { adapter } : {});
+export const prisma = globalForPrisma.prisma || new PrismaClient({});
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
