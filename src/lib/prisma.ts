69609f0ba00293d89181304ceeb557904c4e32ce
king6870 <kingjacobisthegoat@gmail.com>
2026-02-24 15:15:06 -0800

    fix: Configure PrismaClient with datasource URL from environment variable

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -22,5 +22,11 @@ logActiveEnvFile();
 
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
